63f9b0ec72240a4367daa8968243a85a49283f27
Nathaniel Ludwig <nathanielludwig.edu@gmail.com>
2024-04-06 01:42:20 -0600

    finished setting up card class. did heavy refactoring, will need to move class to its own file and export

--- pages/index.js
+++ pages/index.js
@@ -24,12 +24,55 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
+///////////////////////////////////////////////////////////////////////////////////////////////
+//test test test//
+//this Card class is a test and substituting for the getCardElement function
 
+class Card {
+  constructor(data, cardSelector, imagePopUpHandler) {
+    this._title = data.title;
+    this._link = data.link;
+    this._cardSelector = cardSelector;
+    this.imagePopUpHandler = imagePopUpHandler;
+  }
+  _getTemplate() {
+    const cardElement = document
+      .querySelector("#card__template")
+      .content.firstElementChild.cloneNode(true);
+    return cardElement;
+  }
+
+  generateCard() {
+    const cardElement = this._getTemplate();
+    cardElement.querySelector(".card__image").src = this._link;
+    cardElement.querySelector(".card__image").alt = this._title;
+    cardElement.querySelector(".card__title").textContent = this._title;
+    this._setEventListeners(cardElement);
+    this.imagePopUpHandler(cardElement);
+    return cardElement;
+  }
+
+  _setEventListeners(cardElement) {
+    cardElement
+      .querySelector(".card__heart-button")
+      .addEventListener("click", () => {
+        cardElement
+          .querySelector(".card__heart-button")
+          .classList.toggle("card__heart-button_active");
+      });
+    cardElement
+      .querySelector(".card__delete-button")
+      .addEventListener("click", function () {
+        cardElement.remove();
+      });
+  }
+}
+////    end   /////
+//test test test//
+///////////////////////////////////////////////////////////////////////////////////////////////
 // elements
 const modals = Array.from(document.querySelectorAll(".modal"));
 const cardGalleryEL = document.querySelector(".gallery");
-const cardTemplate =
-  document.querySelector("#card__template").content.firstElementChild;
 const modalPictureImageEl = document.querySelector(".modal-picture__image");
 const modalPictureTitleEl = document.querySelector(".modal-picture__title");
 const profileName = document.querySelector(".profile__name");
@@ -64,7 +107,6 @@ const newPlaceModal = document.querySelector(".modal_new-place");
 const profileEditModal = document.querySelector(".modal_profile-edit");
 const modalPicture = document.querySelector(".modal_picture");
 // functions
-
 function openModal(modal) {
   modal.classList.add("modal_opened");
   document.addEventListener("keydown", closeModalWithEsc);
@@ -83,39 +125,52 @@ function updateProfileInfo() {
   profileSubtitle.textContent = inputSubtitle.value;
   closeModal(profileEditModal);
 }
-function getCardElement(cardData) {
-  const cardElement = cardTemplate.cloneNode(true);
-  const cardImageEl = cardElement.querySelector(".card__image");
-  const cardTitleEl = cardElement.querySelector(".card__title");
-  const heartBtn = cardElement.querySelector(".card__heart-button");
-  const deleteCardBtn = cardElement.querySelector(".card__delete-button");
-  cardTitleEl.textContent = cardData.title;
-  cardImageEl.setAttribute("src", cardData.link);
-  cardImageEl.setAttribute("alt", cardData.title);
-  heartBtn.addEventListener("click", function () {
-    heartBtn.classList.toggle("card__heart-button_active");
-  });
-  deleteCardBtn.addEventListener("click", function () {
-    cardElement.remove();
-  });
-  cardImageEl.addEventListener("click", function () {
-    openModal(modalPicture);
-    modalPictureImageEl.setAttribute("src", cardData.link);
-    modalPictureImageEl.setAttribute("alt", cardData.title);
-    modalPictureTitleEl.textContent = cardData.title;
-  });
-  return cardElement;
-}
-function addNewCard() {
-  const newCard = {};
-  newCard.title = cardTitleInput.value;
-  newCard.link = cardImageInput.value;
-  const newCardElement = getCardElement(newCard);
-  cardGalleryEL.prepend(newCardElement);
-}
+
 function clearNewPlaceInfo() {
   cardImageInput.value = "";
   cardTitleInput.value = "";
+}
+
+function addNewCard() {
+  const cardData = {};
+  const cardTitleInput = document.querySelector(
+    ".modal__text-input_type_title"
+  );
+  const cardImageInput = document.querySelector(
+    ".modal__text-input_type_image"
+  );
+  cardData.title = cardTitleInput.value;
+  cardData.link = cardImageInput.value;
+  const newCard = new Card(cardData, "#card__template", imageModalHandler);
+  const readyCard = newCard.generateCard();
+  cardGalleryEL.prepend(readyCard);
+}
+
+function closeModalWithEsc(e) {
+  const key = e.code;
+  if (key === "Escape") {
+    const openModal = document.querySelector(".modal_opened");
+    closeModal(openModal);
+  }
+}
+
+function createCard(data) {
+  const card = new Card(data, "#card__template", imageModalHandler);
+  const cardFinal = card.generateCard();
+  cardGalleryEL.prepend(cardFinal);
+}
+
+function imageModalHandler(cardElement) {
+  cardElement
+    .querySelector(".card__image")
+    .addEventListener("click", function () {
+      const pictureEle = document.querySelector(".modal-picture__image");
+      const pictureTitle = document.querySelector(".modal-picture__title");
+      pictureEle.src = this.src;
+      pictureEle.atl = this.textContent;
+      pictureTitle.textContent = cardElement.textContent;
+      openModal(modalPicture);
+    });
 }
 //events
 profileForm.addEventListener("submit", function (event) {
@@ -140,13 +195,7 @@ addNewImageBtn.addEventListener("click", function () {
   openModal(newPlaceModal);
   toggleButtonState(newPlaceForm, newPlaceInputsArray);
 });
-
 // loops
-initialCards.forEach((cardData) => {
-  const newCardElement = getCardElement(cardData);
-  cardGalleryEL.prepend(newCardElement);
-});
-
 modals.forEach(function (modal) {
   modal.addEventListener("mousedown", function (evt) {
     if (evt.target.classList.contains("modal_opened")) {
@@ -158,10 +207,6 @@ modals.forEach(function (modal) {
   });
 });
 
-function closeModalWithEsc(e) {
-  const key = e.code;
-  if (key === "Escape") {
-    const openModal = document.querySelector(".modal_opened");
-    closeModal(openModal);
-  }
-}
+initialCards.forEach((data) => {
+  createCard(data);
+});
